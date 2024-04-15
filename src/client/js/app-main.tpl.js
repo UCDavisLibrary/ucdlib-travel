c0d3446f4a0327c1477de4b8a1bb4dd72d5510c6
Sabrina Baggett <sabaggett@ucdavis.edu>
2024-04-15 14:19:06 -0700

    issue #3 app routes

--- src/client/js/app-main.tpl.js
+++ src/client/js/app-main.tpl.js
@@ -10,9 +10,21 @@ return html`
     </ucdlib-branding-bar>
 
     <!-- TODO: Replace these with your own primary nav links -->
+
     <ucd-theme-primary-nav>
-      <a href='/foo'>Foo</a>
+      <a href='/request/new'>Submit a Request</a>
+      <a href='/request'>Your Submitted Request</a>
+      <a href='/approver'>Approve a Request</a>
+      <a href='/reports'>Reports</a>
     </ucd-theme-primary-nav>
+
+    <ucd-theme-quick-links title="Application Administration" style-modifiers="highlight">
+      <a href="/admin#appovers">Approvers and Funding Sources</a>
+      <a href="/admin#settings">General Settings</a>
+      <a href="/admin#allocations">Employee Allocations</a>
+      <a href="/admin#items">Line Items</a>
+      <a href="/reimbursement">Reimbursement Requests</a>
+    </ucd-theme-quick-links>
   </ucd-theme-header>
   <!-- <ucdlib-iam-alert></ucdlib-iam-alert> -->
 
@@ -32,7 +44,14 @@ return html`
   <ucdlib-pages id='main-pages' selected=${this.page}>
     <app-page-alt-state id=${this._notLoadedPageId} .state=${this.pageState} .errorMessage=${this.errorMessage}></app-page-alt-state>
     <app-page-home id='home'></app-page-home>
-    <app-page-foo id='foo'></app-page-foo>
+    <app-page-admin id='admin'></app-page-admin>
+    <app-page-approver id='approver'></app-page-approver>
+    <app-page-reimbursement id='reimbursement'></app-page-reimbursement>
+    <app-page-reimbursement-new id='reimbursement-new'></app-page-reimbursement-new>
+    <app-page-reports id='reports'></app-page-reports>
+    <app-page-request id='request'></app-page-request>
+    <app-page-request-new id='request-new'></app-page-request-new>
+
   </ucdlib-pages>
 
 `;}
---
+++ src/client/js/pages/app-page-request-new.js
@@ -0,0 +1,22 @@
+import { LitElement } from 'lit';
+import {render} from "./app-page-request-new.tpl.js";
+import { LitCorkUtils, Mixin } from "../../../lib/appGlobals.js";
+import { MainDomElement } from "@ucd-lib/theme-elements/utils/mixins/main-dom-element.js";
+
+export default class AppPageRequestNew extends Mixin(LitElement)
+.with(LitCorkUtils, MainDomElement) {
+
+  static get properties() {
+    return {
+      
+    }
+  }
+
+  constructor() {
+    super();
+    this.render = render.bind(this);
+  }
+
+}
+
+customElements.define('app-page-request-new', AppPageRequestNew);
