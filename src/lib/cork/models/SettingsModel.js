de5c44046a1a11c5503d72c971ed1d963946f3ea
spelkey-ucd <spelkey@ucdavis.edu>
2024-04-26 14:56:53 -0400

    #27

--- src/lib/cork/models/SettingsModel.js
+++ src/lib/cork/models/SettingsModel.js
@@ -31,6 +31,28 @@ class SettingsModel extends BaseModel {
   }
 
   /**
+   * @description Get a settings value by key from the settings store
+   * It must have aleady been fetched as part of a getByCategory call
+   * @param {String} key - key of setting to get
+   * @param {String} defaultValue - default value to return if setting does not exist
+   * @returns {String} setting value or default value if setting does not exist
+   */
+  getByKey(key, defaultValue=''){
+    for( let category in this.store.data.byCategory ) {
+      let settings = this.store.data.byCategory[category].payload;
+      if( settings && Array.isArray(settings) ) {
+        for( let setting of settings ) {
+          if( setting.key === key ) {
+            return setting.useDefaultValue ? setting.defaultValue : setting.value;
+          }
+        }
+      }
+    }
+    return defaultValue;
+
+  }
+
+  /**
    * @description Clear browser cache for all categories or a specific category
    * @param {String} category - category to clear cache for, if not provided all categories will be cleared
    * @returns {Boolean} true if cache was cleared, false if category cache did not exist
