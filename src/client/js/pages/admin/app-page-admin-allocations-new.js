ef46528dc74f2cb1f66db36344bc3fa5f11ba17b
spelkey-ucd <spelkey@ucdavis.edu>
2024-05-07 14:46:39 -0400

    #36

--- src/client/js/pages/admin/app-page-admin-allocations-new.js
+++ src/client/js/pages/admin/app-page-admin-allocations-new.js
@@ -4,19 +4,25 @@ import { LitCorkUtils, Mixin } from "../../../../lib/appGlobals.js";
 import { MainDomElement } from "@ucd-lib/theme-elements/utils/mixins/main-dom-element.js";
 import { WaitController } from "@ucd-lib/theme-elements/utils/controllers/wait.js";
 import { createRef } from 'lit/directives/ref.js';
+import IamEmployeeObjectAccessor from '../../../../lib/utils/iamEmployeeObjectAccessor.js';
 
 export default class AppPageAdminAllocationsNew extends Mixin(LitElement)
 .with(LitCorkUtils, MainDomElement) {
 
   static get properties() {
     return {
-
+      employees: {type: Array},
+      startDate: {type: String},
+      endDate: {type: String}
     }
   }
 
   constructor() {
     super();
     this.render = render.bind(this);
+    this.employees = [];
+    this.startDate = '';
+    this.endDate = '';
 
     this.employeeSearchRef = createRef();
     this.waitController = new WaitController(this);
@@ -53,8 +59,45 @@ export default class AppPageAdminAllocationsNew extends Mixin(LitElement)
     this.requestUpdate();
   }
 
+  /**
+   * @description Event handler for when an employee is removed from the list
+   * @param {Object} employee - employee object
+   */
+  _onRemoveEmployee(employee){
+    this.employees = this.employees.filter(e => e.kerberos !== employee.kerberos);
+  }
+
+  /**
+   * @description Event handler for when employees are selected from the employee search component
+   * @param {CustomEvent} e - employee-select event from ucdlib-employee-search-advanced
+   */
   _onEmployeeSelect(e) {
-    console.log('Employee selected', e.detail);
+    const newEmployees = [];
+    for( let employee of e.detail ) {
+      employee = (new IamEmployeeObjectAccessor(employee)).travelAppObject;
+      if( this.employeeIsSelected(employee) ) continue;
+      newEmployees.push(employee);
+    }
+    this.employees = [...this.employees, ...newEmployees];
+  }
+
+  /**
+   * @description Check if an employee is already in the selected list
+   * @param {Object} employee - employee object
+   * @returns
+   */
+  employeeIsSelected(employee) {
+    return this.employees.find(e => e.kerberos === employee.kerberos);
+  }
+
+  _onFormSubmit(e) {
+    e.preventDefault();
+    console.log('submit');
+  }
+
+  _onFormInput(prop, value){
+    this[prop] = value;
+    this.requestUpdate();
   }
 
   /**
--- src/client/js/pages/admin/app-page-admin-allocations-new.tpl.js
+++ src/client/js/pages/admin/app-page-admin-allocations-new.tpl.js
@@ -4,23 +4,93 @@ import { ref } from 'lit/directives/ref.js';
 import "../../components/ucdlib-employee-search-advanced.js";
 
 export function render() {
-return html`
-<div class='l-gutter u-space-mb--large'>
-  <div class='l-basic--flipped'>
-    <div class ='l-content'></div>
-    <div class='l-sidebar-second'>
-    <div class="panel panel--icon panel--icon-custom panel--icon-secondary">
-      <h2 class="panel__title"><span class="panel__custom-icon fa-solid fa-magnifying-glass"></span>Search Library Employees</h2>
-      <section>
-        <ucdlib-employee-search-advanced
-          ${ref(this.employeeSearchRef)}
-          .selectButtonText=${'Add to Allocation List'}
-          @employee-select=${this._onEmployeeSelect}
-          .clearOnSelectConfirmation=${true}>
-        </ucdlib-employee-search-advanced>
-      </section>
-    </div>
+
+  return html`
+  <div class='l-gutter u-space-mb--large'>
+    <div class='l-basic--flipped'>
+      <div class ='l-content'>
+        ${renderForm.call(this)}
+      </div>
+      <div class='l-sidebar-second'>
+      <div class="panel panel--icon panel--icon-custom panel--icon-secondary">
+        <h2 class="panel__title"><span class="panel__custom-icon fa-solid fa-magnifying-glass"></span>Search Library Employees</h2>
+        <section>
+          <ucdlib-employee-search-advanced
+            ${ref(this.employeeSearchRef)}
+            .selectButtonText=${'Add to Allocation List'}
+            @employee-select=${this._onEmployeeSelect}
+            .clearOnSelectConfirmation=${true}>
+          </ucdlib-employee-search-advanced>
+        </section>
+      </div>
+      </div>
     </div>
   </div>
-</div>
-`;}
+  `;}
+
+  function renderForm(){
+    const page = 'app-page-admin-allocations-new';
+
+    return html`
+    <form @submit=${this._onFormSubmit}>
+      <fieldset>
+        <legend>Allocation</legend>
+        <div class="l-2col">
+          <div class="l-first">
+            <div class="field-container">
+              <label for="${page}--from">From <abbr title="Required">*</abbr></label>
+              <input
+                type="date"
+                id="${page}--from"
+                @change=${e => this._onFormInput('startDate', e.target.value)}
+                >
+            </div>
+          </div>
+          <div class="l-second">
+            <div class="field-container">
+              <label for="${page}--to">To <abbr title="Required">*</abbr></label>
+              <input
+                type="date"
+                id="${page}--to"
+                @change=${e => this._onFormInput('endDate', e.target.value)}
+                >
+            </div>
+          </div>
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>Employees</legend>
+        <div ?hidden=${this.employees.length > 0}>
+          <p>No employees selected. Use search form to add employees to this list.</p>
+        </div>
+        <div ?hidden=${!this.employees.length} class='employee-list'>
+          <div class="l-2col employee-list--header">
+            <div class="l-first flex">
+              <div class='x-container'></div>
+              <div>Name</div>
+            </div>
+            <div class="l-second">Department</div>
+          </div>
+          ${this.employees.map(employee => html`
+            <div class="l-2col employee-list--item">
+              <div class="l-first flex">
+                <div class='x-container'>
+                  <a
+                  title='Remove employee'
+                  @click=${e => this._onRemoveEmployee(employee)}
+                  class='icon-link'><i class="double-decker fa-solid fa-xmark"></i></a>
+                </div>
+                <div class='name'>${employee.firstName} ${employee.lastName}</div>
+              </div>
+              <div class="l-second flex">
+                <div class='x-container'></div>
+                <div>${employee?.department?.label || ''}</div>
+              </div>
+            </div>
+          `)}
+        </div>
+      </fieldset>
+      <button type="submit" class='btn btn--primary'>Add Allocations</button>
+    </form>
+    `;
+  }
