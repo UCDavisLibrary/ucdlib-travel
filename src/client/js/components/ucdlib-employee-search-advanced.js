6da6baa3527341e189efd15d6a743fafafa9313e
spelkey-ucd <spelkey@ucdavis.edu>
2024-05-07 09:58:34 -0400

    #34

--- src/client/js/components/ucdlib-employee-search-advanced.js
+++ src/client/js/components/ucdlib-employee-search-advanced.js
@@ -3,6 +3,28 @@ import {render} from "./ucdlib-employee-search-advanced.tpl.js";
 import { LitCorkUtils, Mixin } from "../../../lib/appGlobals.js";
 import { MainDomElement } from "@ucd-lib/theme-elements/utils/mixins/main-dom-element.js";
 
+/**
+ * @description Advanced employee search component.
+ * Allows for searching the Library personnel database by name, department, and title code.
+ * And selecting multiple employees to add to a list.
+ * @param {Array} departments - array of department objects retrieved from DepartmentModel
+ * @param {Array} titleCodes - array of title code objects retrieved from EmployeeModel
+ * @param {Array} selectedDepartments - array of selected department ids
+ * @param {Array} selectedTitleCodes - array of selected title codes
+ * @param {String} employeeName - search input for employee name
+ * @param {Number} page - current page number of search results
+ * @param {Number} maxPage - total number of pages of search results
+ * @param {Array} results - array of employee objects returned from search
+ * @param {Array} selectedEmployees - array of selected employee objects
+ * @param {String} selectButtonText - text for the select confirmation button at the bottom of the component
+ * @param {Boolean} clearOnSelectConfirmation - clear form and results on select confirmation
+ * @param {Boolean} _initialized - component initialization status
+ * @param {Boolean} _formDisabled - form is disabled
+ * @param {Boolean} _searching - search is in progress
+ * @param {Boolean} _error - error occurred during previous search
+ * @param {Boolean} _didSearch - search has been performed
+ * @param {Boolean} _allSelected - all displayed employees are selected
+ */
 export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
 .with(LitCorkUtils, MainDomElement) {
 
@@ -14,6 +36,7 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
       selectedTitleCodes: {type: Array},
       employeeName: {type: String},
       page: {type: Number},
+      maxPage: {type: Number},
       results: {type: Array},
       selectedEmployees: {type: Array},
       selectButtonText: {type: String},
@@ -23,7 +46,7 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
       _searching: {state: true},
       _error: {state: true},
       _didSearch: {state: true},
-      _allSelected: {state: true},
+      _allSelected: {state: true}
     }
   }
 
@@ -35,6 +58,7 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
     this.selectedDepartments = [];
     this.selectedTitleCodes = [];
     this.page = 1;
+    this.maxPage = 1;
     this.employeeName = '';
     this.results = [];
     this.selectedEmployees = [];
@@ -103,6 +127,11 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
     return resolvedPromises;
   }
 
+  /**
+   * @description Handle form submission. Queries the employee model with the form data.
+   * @param {Event} e - form submit event
+   * @returns
+   */
   async _onFormSubmit(e){
     e.preventDefault();
     if ( this._searching || this._formDisabled ) return;
@@ -110,6 +139,7 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
     this._error = false;
     let results = [];
     let maxPage = 1;
+    this.page = 1;
     this._didSearch = false;
 
     try {
@@ -129,6 +159,7 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
     this.results = results;
     this.maxPage = maxPage;
     this._didSearch = true;
+    this.selectedEmployees = [];
   }
 
   /**
@@ -144,10 +175,19 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
     return q;
   }
 
+  /**
+   * @description Check if an employee is selected
+   * @param {Object} employee - employee object retrieved from model
+   * @returns {Boolean}
+   */
   _employeeIsSelected(employee){
     return this.selectedEmployees.some(e => e.user_id === employee.user_id);
   }
 
+  /**
+   * @description Handle individual employee select toggle
+   * @param {Object} employee - employee object retrieved from model
+   */
   _onEmployeeSelectToggle(employee){
     if( this._employeeIsSelected(employee) ) {
       this.selectedEmployees = this.selectedEmployees.filter(e => e.user_id !== employee.user_id);
@@ -156,6 +196,11 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
     }
   }
 
+  /**
+   * @description Get the department name of an employee
+   * @param {Object} employee - employee object retrieved from model
+   * @returns {String}
+   */
   _getEmployeeDepartment(employee){
     let out = '';
     for (const group of (employee.groups || [])) {
@@ -164,8 +209,23 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
     return out;
   }
 
-  _onSelectAllToggle(){}
+  /**
+   * @description Handle select all toggle. Removes or adds all displayed employees to selectedEmployees array
+   */
+  _onSelectAllToggle(){
+    const displayedEmployeeIds = this.results.map(e => e.user_id);
 
+    if ( this._allSelected ) {
+      this.selectedEmployees = this.selectedEmployees.filter(e => !displayedEmployeeIds.includes(e.user_id));
+    } else {
+      const newlySelectedEmployees = this.results.filter(e => !this._employeeIsSelected(e));
+      this.selectedEmployees = [...this.selectedEmployees, ...newlySelectedEmployees];
+    }
+  }
+
+  /**
+   * @description Handle "Add" button click. Dispatches employee-select event with selected employees
+   */
   _onSelectConfirmation(){
     this.dispatchEvent(new CustomEvent('employee-select', {
       detail: this.selectedEmployees
@@ -175,7 +235,51 @@ export default class UcdlibEmployeeSearchAdvanced extends Mixin(LitElement)
       this.selectedEmployees = [];
       this.results = [];
       this._didSearch = false;
+      this.clearForm();
     }
+  }
+
+  /**
+   * @description Clear form inputs
+   */
+  clearForm(){
+    this.employeeName = '';
+    this.selectedDepartments = [];
+    this.selectedTitleCodes = [];
+  }
+
+  /**
+   * @description Handle pagination change. Queries the employee model with the new page number
+   * @param {Number} newPage - new page number
+   * @returns
+   */
+  async _onPaginationChange(newPage){
+    if ( this._searching ) return;
+
+    this._searching = true;
+    this._error = false;
+    let results = [];
+    let maxPage = 1;
+    this.page = newPage;
+    this._didSearch = false;
+
+    try {
+      const response = await this.EmployeeModel.queryIam(this.getQueryObject());
+      if ( response.state === 'loaded' ) {
+        results = response.payload.data;
+        maxPage = response.payload.totalPages;
+      } else {
+        this._error = true;
+      }
+    } catch(e) {
+      console.error(e);
+      this._error = true;
+    }
+
+    this._searching = false;
+    this.results = results;
+    this.maxPage = maxPage;
+    this._didSearch = true;
   }
 
 }
