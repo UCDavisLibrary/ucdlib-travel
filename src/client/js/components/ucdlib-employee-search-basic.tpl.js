d052e0de4e729907fec17cdac4864bbbe5ecd88c
Mark Warren <mjwarren@ucdavis.edu>
2024-05-08 14:39:31 -0700

    add css

--- src/client/js/components/ucdlib-employee-search-basic.tpl.js
+++ src/client/js/components/ucdlib-employee-search-basic.tpl.js
@@ -5,6 +5,78 @@ export function styles() {
     :host {
       display: block;
     }
+    ucdlib-employee-search-basic {
+      max-width: 500px;
+      margin: auto;
+      display: block
+  }
+  
+  ucdlib-employee-search-basic .emp-search-top.field-container {
+      margin-bottom: 0
+  }
+  
+  ucdlib-employee-search-basic .emp-search-bar {
+      position: relative
+  }
+  
+  ucdlib-employee-search-basic .emp-search-bar input {
+      flex-grow: 1;
+      padding-right: 2rem
+  }
+  
+  ucdlib-employee-search-basic .emp-search-bar .emp-search-icon {
+      position: absolute;
+      width: 2.5rem;
+      min-width: 2.5rem;
+      height: 2.5rem;
+      min-height: 2.5rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      right: 0;
+      top: 0;
+      color: #022851
+  }
+  
+  ucdlib-employee-search-basic .emp-search-results-parent {
+      position: relative
+  }
+  
+  ucdlib-employee-search-basic .emp-search-results {
+      position: absolute;
+      top: 100%;
+      left: 0;
+      right: 0;
+      z-index: 100;
+      background-color: #fffbed;
+      border-right: 1px solid #ffbf00;
+      border-left: 1px solid #ffbf00;
+      border-bottom: 1px solid #ffbf00;
+      color: #13639e;
+      max-height: 250px;
+      overflow-y: scroll
+  }
+  
+  ucdlib-employee-search-basic .emp-search-results .emp-search-result {
+      padding: .5rem .75rem
+  }
+  
+  ucdlib-employee-search-basic .emp-search-results .emp-search-result:hover {
+      background-color: #fde9ac
+  }
+  
+  ucdlib-employee-search-basic .emp-search-results .highlight {
+      font-weight: 700
+  }
+  
+  ucdlib-employee-search-basic .emp-search-results .muted {
+      font-weight: 400;
+      color: #545454
+  }
+  
+  ucdlib-employee-search-basic .emp-search-results .emp-search-more {
+      padding: .5rem .75rem
+  }
   `;
 
   return [elementStyles];
@@ -22,10 +94,11 @@ return html`
         @focus=${() => this.isFocused = true}
         @blur=${() => this._onBlur()}
         .value=${this.selectedText ? this.selectedText : this.query}
+        placeholder=${this.labelText}
         type="text">
         <div class='emp-search-icon'>
           <div ?hidden=${this.status != 'idle'} >
-            <i class='fas fa-search'></i>
+            <i class='fas fa-regular fa-user'></i>
           </div>
           <div ?hidden=${this.status != 'searching'}>
             <i class="fas fa-circle-notch fa-spin"></i>
@@ -34,7 +107,7 @@ return html`
             <i class="fas fa-exclamation double-decker"></i>
           </div>
           <div ?hidden=${this.status != 'selected'}>
-            <i class="fas fa-check quad"></i>
+            <i class="fas fa-solid fa-user"></i>
           </div>
         </div>
     </div>
