0ad7f5c49ad9a4353726d9876d8c7a36908635a2
Mark Warren <mjwarren@ucdavis.edu>
2024-05-09 18:39:07 -0700

    remove inline css, change result and query object results

--- src/client/js/components/ucdlib-employee-search-basic.tpl.js
+++ src/client/js/components/ucdlib-employee-search-basic.tpl.js
@@ -1,16 +1,5 @@
 import { html, css } from 'lit';
 
-export function styles() {
-  const elementStyles = css`
-    :host {
-      display: block;
-    }
-    
-  `;
-
-  return [elementStyles];
-}
-
 export function render() {
 return html`
 <div>
