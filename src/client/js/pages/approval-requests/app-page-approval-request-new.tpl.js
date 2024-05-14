9e65e7eccab097464be78da86a10db34518d55bf
Mark Warren <mjwarren@ucdavis.edu>
2024-05-14 13:25:21 -0700

    Update app-page-approval-request-new.tpl.js

--- src/client/js/pages/approval-requests/app-page-approval-request-new.tpl.js
+++ src/client/js/pages/approval-requests/app-page-approval-request-new.tpl.js
@@ -2,10 +2,4 @@ import { html } from 'lit';
 
 export function render() {
 return html`
-    <form>
-        <ucdlib-employee-search-basic
-        class='u-space-mb'
-        @status-change=${this._onTransferEmployeeStatusChange}>
-        </ucdlib-employee-search-basic>
-    </form>
 `;}
