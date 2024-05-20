5aeef8e4a1a2f0374aec100c6a7776ec734e18ea
spelkey-ucd <spelkey@ucdavis.edu>
2024-05-20 16:31:45 -0400

    #44

--- src/client/js/pages/approval-requests/app-page-approval-request-new.js
+++ src/client/js/pages/approval-requests/app-page-approval-request-new.js
@@ -3,12 +3,15 @@ import {render} from "./app-page-approval-request-new.tpl.js";
 import { LitCorkUtils, Mixin } from "../../../../lib/appGlobals.js";
 import { MainDomElement } from "@ucd-lib/theme-elements/utils/mixins/main-dom-element.js";
 
+import ValidationHandler from "../../utils/ValidationHandler.js";
+
 export default class AppPageApprovalRequestNew extends Mixin(LitElement)
 .with(LitCorkUtils, MainDomElement) {
 
   static get properties() {
     return {
-
+      revisionId: {type: Number},
+      label: {type: String}
     }
   }
 
@@ -16,7 +19,10 @@ export default class AppPageApprovalRequestNew extends Mixin(LitElement)
     super();
     this.render = render.bind(this);
 
-    this._injectModel('AppStateModel');
+    this.revisionId = 0;
+    this.settingsCategory = 'approval-requests';
+
+    this._injectModel('AppStateModel', 'SettingsModel');
   }
 
   /**
@@ -25,6 +31,7 @@ export default class AppPageApprovalRequestNew extends Mixin(LitElement)
    */
   async _onAppStateUpdate(state) {
     if ( this.id !== state.page ) return;
+    this.AppStateModel.showLoading();
 
     this.AppStateModel.setTitle('New Approval Request');
 
@@ -34,6 +41,51 @@ export default class AppPageApprovalRequestNew extends Mixin(LitElement)
       this.AppStateModel.store.breadcrumbs[this.id]
     ];
     this.AppStateModel.setBreadcrumbs(breadcrumbs);
+
+    this._setRevisionId(state);
+
+    const d = await this.getPageData();
+    const hasError = d.some(e => e.status === 'rejected' || e.value.state === 'error');
+    if ( hasError ) {
+      this.AppStateModel.showError(d);
+      return;
+    }
+
+    this.AppStateModel.showLoaded(this.id);
+    this.requestUpdate();
+  }
+
+  /**
+   * @description Get all data required for rendering this page
+   */
+  async getPageData(){
+    const promises = [];
+    promises.push(this.SettingsModel.getByCategory(this.settingsCategory));
+    const resolvedPromises = await Promise.allSettled(promises);
+    return resolvedPromises;
+  }
+
+  async _onSubmit(e){
+    e.preventDefault();
+    console.log('submit');
+  }
+
+  /**
+   * @description Reset form state
+   */
+  resetForm(){
+    this.label = '';
+    this.validationHandler = new ValidationHandler();
+    this.requestUpdate();
+  }
+
+  /**
+   * @description Set revisionId property from App State location (the url)
+   * @param {Object} state - AppStateModel state
+   */
+  _setRevisionId(state) {
+    let revisionId = Number(state?.location?.path?.[2]);
+    this.revisionId = Number.isInteger(revisionId) && revisionId > 0 ? revisionId : 0;
   }
 
 }
