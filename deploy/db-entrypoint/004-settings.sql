ac61cef2f2a2f2645582f0f1f535a509e0ab8214
spelkey-ucd <spelkey@ucdavis.edu>
2024-05-20 16:31:45 -0400

    #44

--- deploy/db-entrypoint/004-settings.sql
+++ deploy/db-entrypoint/004-settings.sql
@@ -1,5 +1,9 @@
 INSERT INTO settings(key, value, label, description, input_type, categories) VALUES ('mileage_rate', 3.50, 'Mileage Rate', 'The current mileage rate for personal car mileage reimbursement.', 'number', '{"approval-request-form", "admin-settings"}');
 
+-- approval requests
+INSERT INTO "settings" ("key", "value", "label", "description", "default_value", "use_default_value", "keywords", "settings_page_order", "input_type", "categories", "can_be_html")
+VALUES ('approval_request_form_intro', '', NULL, 'Shown on the top of the approval request form.', 'Use the following form to request approval for your travel, training, or professional development.', '1', NULL, '10', 'textarea', '{approval-requests,admin-settings}', '1');
+
 -- admin line items page
 INSERT INTO "settings" ("key", "value", "label", "description", "default_value", "use_default_value", "keywords", "settings_page_order", "input_type", "categories", "can_be_html")
 VALUES ('admin_line_items_description', '', 'Admin - Line Items Description', 'Displays on top of line item admin settings page', 'Requesters will be able to select and assign monetary values to the following line items when submitting an approval form', '1', NULL, '100', 'textarea', '{admin-line-items,admin-settings}', '1');
--- src/client/js/pages/approval-requests/app-page-approval-request-new.tpl.js
+++ src/client/js/pages/approval-requests/app-page-approval-request-new.tpl.js
@@ -1,6 +1,19 @@
 import { html } from 'lit';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 
 export function render() {
 return html`
+  <div class='l-gutter u-space-mb--large'>
+    <div class='l-basic--flipped'>
+      <div class ='l-content'>
+        <p>${unsafeHTML(this.SettingsModel.getByKey('approval_request_form_intro'))}</p>
+        <form class='skinny-form'>
+          <p>hello world</p>
+        </form>
+      </div>
+      <div class='l-sidebar-second'>
+      </div>
+    </div>
+  </div>
 
 `;}
