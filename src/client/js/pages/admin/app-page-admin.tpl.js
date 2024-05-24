671dcdbd06f7ee876aa3c4cf077a48f40f90260f
Mark Warren <mjwarren@ucdavis.edu>
2024-05-24 13:04:40 -0700

    initial commit

--- src/client/js/pages/admin/app-page-admin.tpl.js
+++ src/client/js/pages/admin/app-page-admin.tpl.js
@@ -2,6 +2,40 @@ import { html } from 'lit';
 
 export function render() { 
 return html`
-
+<div class="l-gutter--narrow panel">
+<section class="article-list">
+<div class="vm-listing ">
+  <div class="vm-listing__figure">
+    <a href=""><img src="../../images/placeholders/135x135.png" alt="Thumbnail" class="" width="135" height="135" loading="lazy">
+</a>
+  </div>
+  <div class="vm-listing__body">
+    <h3 class="vm-listing__title"><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. (72 characters)</a></h3>
+      <div class="vm-listing__submitted">February 10,  2021</div>
+  </div>
+</div>
+<div class="vm-listing ">
+  <div class="vm-listing__figure">
+    <a href=""><img src="../../images/placeholders/135x135.png" alt="Thumbnail" class="" width="135" height="135" loading="lazy">
+</a>
+  </div>
+  <div class="vm-listing__body">
+    <h3 class="vm-listing__title"><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. (72 characters)</a></h3>
+      <div class="vm-listing__submitted">February 10,  2021</div>
+  </div>
+</div>
+<div class="vm-listing ">
+  <div class="vm-listing__figure">
+    <a href=""><img src="../../images/placeholders/135x135.png" alt="Thumbnail" class="" width="135" height="135" loading="lazy">
+</a>
+  </div>
+  <div class="vm-listing__body">
+    <h3 class="vm-listing__title"><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. (72 characters)</a></h3>
+      <div class="vm-listing__submitted">February 10,  2021</div>
+  </div>
+</div>
+<a href="#" class="icon icon--circle-arrow-right">View All</a>
+</section>
+  </div>
 
 `;}
