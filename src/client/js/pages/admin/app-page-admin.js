becbeffdc444dfb8994294d1917ae4187b985061
Mark Warren <mjwarren@ucdavis.edu>
2024-05-30 17:51:57 -0700

    add content to page

--- src/client/js/pages/admin/app-page-admin.js
+++ src/client/js/pages/admin/app-page-admin.js
@@ -6,14 +6,12 @@ import { MainDomElement } from "@ucd-lib/theme-elements/utils/mixins/main-dom-el
 /**
  * @description Admin home page
  * @param {Array} adminPages - local copy of active page objects from AdminPagesModel
- * @param {Object} newAdminPage - new admin page object being created
  */
 export default class AppPageAdmin extends Mixin(LitElement)
 .with(LitCorkUtils, MainDomElement) {
   static get properties() {
     return {
       adminPages: {type: Array},
-      newAdminPage: {type: Object},
     }
   }
 
@@ -21,23 +19,11 @@ export default class AppPageAdmin extends Mixin(LitElement)
   constructor() {
     super();
     this.render = render.bind(this);
-    this.settingsCategory = 'admin-pages';
+    this.settingsCategory = 'admin-page';
     this.adminPages = [];
-    this.newAdminPage = {};
 
-    this.waitController = new WaitController(this);
-
-    this._injectModel('AppStateModel','SettingsModel','AdminPagesModel');
+    this._injectModel('AppStateModel','SettingsModel');
   }
-
-    /**
-   * @description lit lifecycle method
-   */
-    willUpdate(changedProps) {
-      if ( changedProps.has('newAdminPage') ) {
-        this.showNewAdminPageForm = this.newAdminPage && Object.keys(this.newAdminPage).length > 0;
-      }
-    }
 
     /**
    * @description bound to AppStateModel app-state-update event
@@ -53,15 +39,24 @@ export default class AppPageAdmin extends Mixin(LitElement)
         this.AppStateModel.store.breadcrumbs.admin
       ];
       this.AppStateModel.setBreadcrumbs(breadcrumbs);
+
+      const d = await this.getPageData();
+      const hasError = d.some(e => e.status === 'rejected' || e.value.state === 'error');
+      if ( hasError ) {
+        this.AppStateModel.showError(d);
+        return;
+      }
+  
+      this.AppStateModel.showLoaded(this.id);
+      this.requestUpdate();
     }
 
-      /**
+  /**
    * @description Get all data required for rendering this page
    */
       async getPageData(){
         const promises = [];
         promises.push(this.SettingsModel.getByCategory(this.settingsCategory));
-        promises.push(this.LineItemsModel.getActiveLineItems());
         const resolvedPromises = await Promise.allSettled(promises);
         return resolvedPromises;
       }
