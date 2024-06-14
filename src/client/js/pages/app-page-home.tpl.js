099910a28bda46a44c640c9873f4e2f8054ef26e
Sabrina Baggett <sabaggett@ucdavis.edu>
2024-06-14 08:06:31 -0700

    stash

--- src/client/js/pages/app-page-home.tpl.js
+++ src/client/js/pages/app-page-home.tpl.js
@@ -1,11 +1,15 @@
 import { html } from 'lit';
 
+import "../components/approval-request-teaser.js";
+
+
 export function render() {
 return html`
   <div class='l-container'>
     <div class='l-basic--flipped'>
       <div class="l-content">
         <p>Home Page</p>
+        <approval-request-teaser></approval-request-teaser>
       </div>
     </div>
   </div>
