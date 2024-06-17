bcdbbac46e8af61356e633f2844a9ac12e275f0a
Sabrina Baggett <sabaggett@ucdavis.edu>
2024-06-17 15:59:11 -0700

    stash

--- src/client/js/components/approval-request-teaser.js
+++ src/client/js/components/approval-request-teaser.js
@@ -26,6 +26,11 @@ export default class ApprovalRequestTeaser extends Mixin(LitElement)
 
   }
 
+  /**
+   * @description formatting the currency to dollar format
+   * @param {Float32Array} dollar -  funding amount 
+   * @returns
+   */
   formatDollar(dollar){
     let dollarFormat = new Intl.NumberFormat('en-US', {
       style: 'currency',
@@ -36,6 +41,11 @@ export default class ApprovalRequestTeaser extends Mixin(LitElement)
     return dollar
   }
 
+  /**
+   * @description formatting the date given
+   * @param {String} date -  date string given
+   * @returns
+   */
   formatDate(date){
     const event = new Date(date);
     date = new Intl.DateTimeFormat('en-US', {
@@ -45,11 +55,32 @@ export default class ApprovalRequestTeaser extends Mixin(LitElement)
     return date;
   }
 
+  /**
+   * @description check the kerberos is not the current one on the page
+   * @param {String} kerb -  approval request kerberos id
+   * @returns
+   */
   checkKerb(kerb){
     if(this.AuthModel.getToken().token.preferred_username == kerb) return true;
     return false;
   }
 
+   /**
+   * @description check the status of the request if approvers
+   * @param {String} status -  status check
+   * @returns
+   */
+    checkApproverStatus(status){
+      // if(this.AuthModel.getToken().token.preferred_username == kerb) return true;
+      // return false;
+    }
+
+  /**
+   * @description change the Status bar text to upper case
+   * @param {String} str -  string given
+   * @param {Array} separators -  array of strings given for seperation
+   * @returns
+   */
   ToUpperCase(str, separators=['-']) {
     separators = separators || [ ' ' ];
     let regex = new RegExp('(^|[' + separators.join('') + '])(\\w)', 'g');
--- src/client/js/components/approval-request-teaser.tpl.js
+++ src/client/js/components/approval-request-teaser.tpl.js
@@ -3,47 +3,49 @@ import { html } from 'lit';
 
 export function render() { 
 return html`
-  
-      <div ?hidden=${!this.approvalRequest} class="l-shrink panel">
+      ${console.log("A:", this.approvalRequest)}
+      <div ?hidden=${!this.approvalRequest}>
             <div>     
-                  <p ?hidden=${!this.approvalRequest.approvalStatus} class="approvalStatus">${this.ToUpperCase(this.approvalRequest.approvalStatus)}</p>
-                  <p ?hidden=${!this.approvalRequest.reimbursementStatus} class="reimbursementStatus">${this.ToUpperCase(this.approvalRequest.reimbursementStatus)}</p>
+                  <p ?hidden=${!this.approvalRequest.approvalStatus} class="approval-status">${this.ToUpperCase(this.approvalRequest.approvalStatus)}</p>
+                  <p ?hidden=${!this.approvalRequest.reimbursementStatus} class="reimbursement-status">${this.ToUpperCase(this.approvalRequest.reimbursementStatus)}</p>
             </div>
 
             <div class="teaser">
-                  <div class="l-2col l-2col--67-33">
-                        <div class="l-first panel">
+                        <div class="teaser-info">
                                     <div ?hidden=${!this.approvalRequest.label} class='title'>${this.approvalRequest.label}</div>
                                     <div ?hidden=${!this.approvalRequest.employeeKerberos} class='requestor'>
                                           ${!this.checkKerb(this.approvalRequest.employeeKerberos) ? 
                                                 html`${this.approvalRequest.employee.firstName} ${this.approvalRequest.employee.lastName}</div>`
                                                 :html``}
                                     </div>
-                                    <div class="panel panel--icon panel--icon-custom panel--icon-rec-pool">
-                                          <p ?hidden=${!this.approvalRequest.locationDetails} class="panel__title"><span class="panel__custom-icon fa-solid fa-building"></span>
-                                                ${this.approvalRequest.locationDetails}
-                                          </p>
+                                          <div  ?hidden=${!this.approvalRequest.locationDetails} class="list-content">
+                                                <span class="icon-image"><i class="fa-solid fa-building"></i></span>
+                                                <p ?hidden=${!this.approvalRequest.locationDetails} class="icon-text">${this.approvalRequest.locationDetails}  </p>
+                                          </div>
+                                          <br />
+                                          <div ?hidden=${!this.approvalRequest.programStartDate}  class="list-content">
+                                                <span class="icon-image"><i class="fa-solid fa-calendar"></i></span>
+                                                <p class="icon-text">
+                                                      ${this.formatDate(this.approvalRequest.programStartDate)}
+                                                      ${this.approvalRequest.programEndDate ? 
+                                                            html`- ${this.formatDate(this.approvalRequest.programEndDate)}`
+                                                            :html``}
+                                                </p>
+                                          </div>
 
 
-                                          <p ?hidden=${!this.approvalRequest.programStartDate} class="panel__title"><span class="panel__custom-icon fa-solid fa-calendar"></span>
-                                                ${this.formatDate(this.approvalRequest.programStartDate)}
-                                                ${this.approvalRequest.programEndDate ? 
-                                                      html`- ${this.formatDate(this.approvalRequest.programEndDate)}`
-                                                      :html``}
-                                          </p>
-                                    </div>
+                                    
                         </div>
-                        <div ?hidden=${!this.approvalRequest.fundingSources} class="l-second panel">
+                        <div ?hidden=${!this.approvalRequest.fundingSources} class="teaser-expenses">
                               <div>
                                     <b>Projected Expenses</b>
-
+                                    <br />                                    
                                     ${this.approvalRequest.fundingSources.map(dep => html`
                                           ${dep.fundingSourceLabel} - ${this.formatDollar(dep.amount)}<br />
                                     `)}
                               </div>
                         </div>
                         
-                  </div>
             </div>
 
                   
