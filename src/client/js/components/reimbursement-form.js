cb45762fff0ff6bbda414650d90e37a928c0f9b3
spelkey-ucd <spelkey@ucdavis.edu>
2024-07-02 16:26:35 -0400

    #62

--- src/client/js/components/reimbursement-form.js
+++ src/client/js/components/reimbursement-form.js
@@ -5,6 +5,7 @@ import { MainDomElement } from "@ucd-lib/theme-elements/utils/mixins/main-dom-el
 
 import { LitCorkUtils, Mixin } from "../../../lib/appGlobals.js";
 import ValidationHandler from "../utils/ValidationHandler.js";
+import reimbursmentExpenses from '../../../lib/utils/reimbursmentExpenses.js';
 
 export default class ReimbursementForm extends Mixin(LitElement)
   .with(LitCorkUtils, MainDomElement) {
@@ -66,6 +67,7 @@ export default class ReimbursementForm extends Mixin(LitElement)
   resetForm() {
     this.reimbursementRequest = {
       label: 'Reimbursement Request',
+      expenses: []
     };
     this.validationHandler = new ValidationHandler();
     this.requestUpdate();
@@ -93,6 +95,79 @@ export default class ReimbursementForm extends Mixin(LitElement)
     let time = iso.split('T')[1];
     return time.split(':').slice(0,2).join(':');
   }
+
+  /**
+   * @description Check if an expense exists for a category or subcategory
+   * @param {String} category - the category of the expense
+   * @param {String} subCategory - optional. the subcategory of the expense
+   * @returns {Boolean}
+   */
+  hasExpense(category, subCategory){
+    const expenses = (this.reimbursementRequest.expenses || []).filter(e => e.category === category);
+    if ( !subCategory ) return expenses.length > 0;
+    return expenses.find(e => (e.details || {}).subCategory === subCategory);
+  }
+
+  /**
+   * @description Toggle data for an expense category.
+   * If the category is not in the reimbursement request, add a blank expense.
+   * If the category is in the reimbursement request, remove all expenses for that category.
+   * @param {String} category - the category of the expense
+   * @param {String} subCategory - optional. the subcategory of the expense
+   */
+  _onExpenseCategoryToggle(category, subCategory){
+    if ( this.hasExpense(category, subCategory) ){
+      if ( subCategory ){
+        this.reimbursementRequest.expenses = this.reimbursementRequest.expenses.filter(e => !(e.category === category && (e.details || {}).subCategory === subCategory));
+      } else {
+        this.reimbursementRequest.expenses = this.reimbursementRequest.expenses.filter(e => e.category !== category);
+      }
+      this.requestUpdate();
+    } else {
+      this.addBlankExpense(category, subCategory);
+    }
+  }
+
+  /**
+   * @description Add a blank expense to the reimbursement request
+   * @param {String} category - the category of the expense
+   * @param {String} subCategory - optional. the subcategory of the expense
+   */
+  addBlankExpense(category, subCategory){
+    const expense = {
+      category,
+      nonce: Math.random().toString(36).substring(3),
+      details: {}
+    };
+    if ( subCategory ) expense.details = {subCategory};
+    this.reimbursementRequest.expenses.push(expense);
+    this.requestUpdate();
+  }
+
+  /**
+   * @description Delete an expense from the reimbursement request
+   * @param {Object} expense - the expense to delete from reimbursementRequest.expenses
+   */
+  deleteExpense(expense){
+    if ( expense.nonce ){
+      this.reimbursementRequest.expenses = this.reimbursementRequest.expenses.filter(e => e.nonce !== expense.nonce);
+    } else if ( expense.reimbursementRequestExpenseId ){
+      this.reimbursementRequest.expenses = this.reimbursementRequest.expenses.filter(e => e.reimbursementRequestExpenseId !== expense.reimbursementRequestExpenseId);
+    }
+    this.requestUpdate();
+  }
+
+  /**
+   * @description Set a property on an object and request an update
+   * @param {Object} object - the object to set the property on
+   * @param {String} prop - the property to set
+   * @param {*} value - the value to set the property to
+   */
+  _setObjectProperty(object, prop, value){
+    object[prop] = value;
+    this.requestUpdate();
+  }
+
 
 
 
--- src/client/js/components/reimbursement-form.tpl.js
+++ src/client/js/components/reimbursement-form.tpl.js
@@ -1,4 +1,5 @@
 import { html } from 'lit';
+import reimbursmentExpenses from '../../../lib/utils/reimbursmentExpenses.js';
 
 export function render() {
   const idPrefix = `reimbursement-form--${this.reimbursementRequest.approval_request_revision_id || 'new'}`;
@@ -87,6 +88,76 @@ export function render() {
         </div>
       </fieldset>
 
+      <fieldset class=${this.validationHandler.errorClass('expenses', reimbursmentExpenses.transportation.value)}>
+        <legend>${reimbursmentExpenses.transportation.label}</legend>
+        <div>${this.validationHandler.renderErrorMessages('expenses', reimbursmentExpenses.transportation.value)}</div>
+
+        ${reimbursmentExpenses.transportation.subCategories.map(category => html`
+          <div class='u-space-mb'>
+            <div class='flex flex--space-between'>
+              <div class='field-container u-space-mr--small'>
+                <div class='checkbox'>
+                  <input
+                    id="${idPrefix}--transportation-${category.value}"
+                    type="checkbox"
+                    .checked=${this.hasExpense(reimbursmentExpenses.transportation.value, category.value)}
+                    @change=${() => this._onExpenseCategoryToggle(reimbursmentExpenses.transportation.value, category.value)} />
+                  <label for="${idPrefix}--transportation-${category.value}" class='bold'>${category.label}</label>
+                </div>
+              </div>
+              <a
+                title='Add ${category.label} Expense'
+                ?hidden=${!this.hasExpense(reimbursmentExpenses.transportation.value, category.value)}
+                @click=${() => this.addBlankExpense(reimbursmentExpenses.transportation.value, category.value)}
+                class='icon-link quad'>
+                <i class="fa-solid fa-circle-plus"></i>
+              </a>
+            </div>
+            <div class='l-gutter'>
+              ${(this.reimbursementRequest.expenses || []).filter(e => e.category === reimbursmentExpenses.transportation.value && (e.details || {}).subCategory === category.value).map(expense => html`
+                <div class='flex u-space-mb'>
+                  <div class='u-space-mr--small'>
+                    <a
+                      title='Delete ${category.label} Expense'
+                      @click=${() => this.deleteExpense(expense)}
+                      class='icon-link double-decker'>
+                      <i class="fa-solid fa-trash-can"></i>
+                    </a>
+                  </div>
+                  <div class='flex--grow'>${renderTransportationExpenseForm.call(this, expense)}</div>
+                </div>
+              `)}
+            </div>
+          </div>
+        `)}
+      </fieldset>
+
+      <fieldset class=${this.validationHandler.errorClass('expenses', reimbursmentExpenses.registrationFee.value)}>
+        <legend>${reimbursmentExpenses.registrationFee.label}</legend>
+        <div>${this.validationHandler.renderErrorMessages('expenses', reimbursmentExpenses.registrationFee.value)}</div>
+        <div class='flex flex--justify-end'>
+          <a class='icon-link' @click=${() => this.addBlankExpense(reimbursmentExpenses.registrationFee.value)}>
+            <i class="fa-solid fa-circle-plus quad"></i>
+            <span>Add Fee</span>
+          </a>
+        </div>
+        <div>
+          ${(this.reimbursementRequest.expenses || []).filter(e => e.category === reimbursmentExpenses.registrationFee.value).map(expense => html`
+            <div class='flex u-space-mb'>
+              <div class='u-space-mr--small'>
+                <a
+                  title='Delete Expense'
+                  @click=${() => this.deleteExpense(expense)}
+                  class='icon-link double-decker'>
+                  <i class="fa-solid fa-trash-can"></i>
+                </a>
+              </div>
+              <div class='flex--grow'>${renderRegistrationFeeForm.call(this, expense)}</div>
+            </div>
+          `)}
+        </div>
+      </fieldset>
+
       <div class='form-buttons alignable-promo__buttons'>
         <button
           type="submit"
@@ -98,3 +169,154 @@ export function render() {
 
 
   `;}
+
+/**
+ * @description Render the registration fee form
+ * @param {Object} expense - the expense object from the reimbursementRequest.expenses array
+ * @returns
+ */
+function renderRegistrationFeeForm(expense){
+  const nonce = expense.nonce || expense.reimbursementRequestExpenseId;
+  const idPrefix = `reimbursement-form--${nonce}`;
+  return html`
+    <div class='l-2col l-2col--33-67'>
+      <div class='l-first'>
+        <div class='field-container'>
+          <label for="${idPrefix}--amount">Amount *</label>
+          <div class='amount input--dollar width-100'>
+            <input
+              id="${idPrefix}--amount"
+              step="0.01"
+              type="number"
+              .value=${expense.amount}
+              @input=${e => this._setObjectProperty(expense, 'amount', Number(e.target.value))} />
+          </div>
+        </div>
+
+      </div>
+      <div class='l-second'>
+        <div class='field-container'>
+          <label for="${idPrefix}--name">Name *</label>
+          <input
+            id="${idPrefix}--name"
+            type="text"
+            .value=${expense.details.name || ''}
+            @input=${e => this._setObjectProperty(expense.details, 'name', e.target.value)} />
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+
+/**
+ * @description Render an individual transportation expense form
+ * @param {Object} expense - the expense object from the reimbursementRequest.expenses array
+ * @returns {TemplateResult}
+ */
+function renderTransportationExpenseForm(expense){
+  const subCategory = (expense?.details || {}).subCategory;
+  if ( !subCategory ) return html``;
+  const nonce = expense.nonce || expense.reimbursementRequestExpenseId;
+  const idPrefix = `reimbursement-form--${nonce}`;
+
+  const roundTripRadioButtons = html`
+    <div class='radio radio--horizontal'>
+      <div>
+        <input
+          id="${idPrefix}--${subCategory}--round-trip"
+          type="radio"
+          .checked=${!expense.details.oneWay}
+          @input=${e => this._setObjectProperty(expense.details, 'oneWay', false)} />
+        <label for="${idPrefix}--${subCategory}--round-trip">Round Trip</label>
+      </div>
+      <div>
+        <input
+          id="${idPrefix}--${subCategory}--one-way"
+          type="radio"
+          .checked=${expense.details.oneWay}
+          @input=${e => this._setObjectProperty(expense.details, 'oneWay', true)} />
+        <label for="${idPrefix}--${subCategory}--one-way">One Way</label>
+      </div>
+    </div>
+  `;
+
+  const addressFields = html`
+    <div class='l-2col'>
+      <div class='l-first'>
+        <div class='field-container'>
+          <label for="${idPrefix}--${subCategory}--from">
+            <span ?hidden=${subCategory !== 'private-car'}>From (Street Address) *</span>
+            <span ?hidden=${subCategory === 'private-car'}>From *</span>
+          </label>
+          <input
+            id="${idPrefix}--${subCategory}--from"
+            type="text"
+            .value=${expense.details.from || ''}
+            @input=${e => this._setObjectProperty(expense.details, 'from', e.target.value)} />
+        </div>
+      </div>
+      <div class='l-second'>
+        <div class='field-container'>
+          <label for="${idPrefix}--${subCategory}--to">
+            <span ?hidden=${subCategory !== 'private-car'}>To (Street Address) *</span>
+            <span ?hidden=${subCategory === 'private-car'}>To *</span>
+          </label>
+          <input
+            id="${idPrefix}--${subCategory}--to"
+            type="text"
+            .value=${expense.details.to || ''}
+            @input=${e => this._setObjectProperty(expense.details, 'to', e.target.value)} />
+        </div>
+      </div>
+    </div>
+  `;
+
+  const amountField = html`
+    <div class='field-container'>
+      <label for="${idPrefix}--${subCategory}--amount">Amount *</label>
+      <div class='amount input--dollar'>
+        <input
+          id="${idPrefix}--${subCategory}--amount"
+          type="number"
+          step="0.01"
+          .value=${expense.amount}
+          @input=${e => this._setObjectProperty(expense, 'amount', Number(e.target.value))} />
+      </div>
+    </div>
+  `;
+
+  if ( subCategory === 'private-car' ){
+    return html`
+      <div>
+        ${roundTripRadioButtons}
+        ${addressFields}
+      </div>
+    `;
+  }
+
+  if ( subCategory === 'airfare-train' ){
+    return html`
+      <div>
+        ${amountField}
+        <div class='u-space-pt--small'>
+          ${roundTripRadioButtons}
+          ${addressFields}
+        </div>
+      </div>
+    `;
+  }
+
+  if ( subCategory === 'rental-car' ){
+    return html`
+      <div>
+        ${amountField}
+        <div class='u-space-pt--small'>
+          ${roundTripRadioButtons}
+          ${addressFields}
+        </div>
+      </div>
+    `;
+  }
+  return html``;
+}
