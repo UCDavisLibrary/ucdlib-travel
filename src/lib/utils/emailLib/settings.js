41a6fd1b0bc0fc81755be56a8369826d7d571d23
Sabrina Baggett <sabaggett@ucdavis.edu>
2024-07-29 14:43:54 -0700

    change to the settings

--- src/lib/utils/emailLib/settings.js
+++ src/lib/utils/emailLib/settings.js
@@ -17,41 +17,31 @@ class Settings {
     return changedType;
   }
 
-  async _getTemplates(type, temp){
+  async _getTemplates(type){
     let changedType = this._changeFormat(type);
     let parse_body = 'admin_email_body_' + changedType;
     let parse_subject = 'admin_email_subject_' + changedType;
 
-    // Quick fix
-    const body = temp.filter((t) => t.key == parse_body);
-    const subject = temp.filter((t) => t.key == parse_subject);
-
 
     /* This is real way to do it */
-    // let body = await dbSettings.getByKey(parse_body);
-    // let subject = await dbSettings.getByKey(parse_subject);
+    let body = await dbSettings.getByKey(parse_body);
+    let subject = await dbSettings.getByKey(parse_subject);
 
-    return [body[0].defaultValue, subject[0].defaultValue]
+
+    return [body.defaultValue, subject.defaultValue]
   }
 
-  async _getEmail(temp){
+  async _getEmail(){
 
     // Quick fix
-    const email = temp.filter((t) => t.key == "admin_email_address");
+    // const email = temp.filter((t) => t.key == "admin_email_address");
 
     /* This is real way to do it */
-    // let email = await dbSettings.getByKey("admin_email_address");
+    let email = await dbSettings.getByKey("admin_email_address");
 
-    return email[0].defaultValue;
+    return email.defaultValue;
   }
 
-//   getNotificationRecipient(type){
-//     this._changeFormat(type);
-//     let system_email = 'admin_email_recipient_' + changedType;
-//     let email =  settings.getByKey(system_email).default_value;
-
-//     return email;
-//   }
 
 
 }
