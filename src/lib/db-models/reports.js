ecaf02c3189df915f53a4bb7c16486cb87de04c6
spelkey-ucd <spelkey@ucdavis.edu>
2024-09-04 10:56:02 -0400

    #93

--- src/lib/db-models/reports.js
+++ src/lib/db-models/reports.js
@@ -1,21 +1,28 @@
 import employeeModel from './employee.js';
+import departmentModel from './department.js';
 import IamEmployeeObjectAccessor from "../utils/iamEmployeeObjectAccessor.js";
 
 class Reports {
 
   constructor(){}
 
+  /**
+   * @description Get the access level of the user
+   * @param {AccessToken} token - The user's access token
+   * @returns {Object} {hasAccess: Boolean, departmentRestrictions: Array}
+   * - hasAccess: Boolean - Whether the user has access to the reports
+   * - departmentRestrictions: Array - An array of department ids that the user has access to. If empty, the user has access to all departments.
+   */
   async getAccessLevel(token){
     const out = {
       hasAccess: false,
       departmentRestrictions: [],
     };
 
-    // todo: uncomment when testing is complete
-    // if ( token.canAccessReports ){
-    //   out.hasAccess = true;
-    //   return out;
-    // }
+    if ( token.canAccessReports ){
+      out.hasAccess = true;
+      return out;
+    }
 
     let employee = await employeeModel.getIamRecordById(token.id)
     if ( employee.error ) {
@@ -30,8 +37,11 @@ class Reports {
     out.hasAccess = true;
     out.departmentRestrictions.push(employee.department.id)
 
-    // todo: get all child departments
-
+    const descendantDepartments = await departmentModel.getAllDescendantDepartments(employee.department.id);
+    if ( descendantDepartments.error ) {
+      return descendantDepartments;
+    }
+    out.departmentRestrictions.push(...descendantDepartments.map(department => department.id));
 
     return out;
   }
