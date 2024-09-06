965872a359a39d940f9bcfa59480725236a4f4e0
spelkey-ucd <spelkey@ucdavis.edu>
2024-09-06 11:37:25 -0400

    #95

--- src/client/js/pages/reports/app-page-reports.js
+++ src/client/js/pages/reports/app-page-reports.js
@@ -5,8 +5,8 @@ import { createRef } from 'lit/directives/ref.js';
 import { LitCorkUtils, Mixin } from '@ucd-lib/cork-app-utils';
 import { MainDomElement } from "@ucd-lib/theme-elements/utils/mixins/main-dom-element.js";
 
-import objectUtils from '../../../../lib/utils/objectUtils.js';
 import fiscalYearUtils from '../../../../lib/utils/fiscalYearUtils.js';
+import reportUtils from '../../../../lib/utils/reports/reportUtils.js';
 
 export default class AppPageReports extends Mixin(LitElement)
 .with(LitCorkUtils, MainDomElement) {
@@ -16,9 +16,9 @@ export default class AppPageReports extends Mixin(LitElement)
       page: {type: String},
       helpUrl: {type: String},
       helpDialogPage: {type: String},
-      filterRows: {state: true},
+      filters: {state: true},
       selectedFilters: {state: true},
-      selectedMeasures: {state: true},
+      selectedMetrics: {state: true},
       selectedAggregatorX: {state: true},
       selectedAggregatorY: {state: true}
     }
@@ -33,13 +33,13 @@ export default class AppPageReports extends Mixin(LitElement)
     this.helpDialogRef = createRef();
     this.helpDialogPage = 'metrics';
 
-    this.filterRows = [];
+    this.filters = [];
     this.selectedFilters = {
       fiscalYear: [fiscalYearUtils.current().startYear]
     };
-    this.selectedMeasures = [];
-    this.selectedAggregatorX = '';
-    this.selectedAggregatorY = '';
+    this.selectedMetrics = reportUtils.defaultMetrics(true);
+    this.selectedAggregatorX = reportUtils.defaultAggregator('x', true);
+    this.selectedAggregatorY = reportUtils.defaultAggregator('y', true);
 
     this._injectModel('AppStateModel', 'ReportsModel', 'SettingsModel');
   }
@@ -101,7 +101,7 @@ export default class AppPageReports extends Mixin(LitElement)
 
   _onReportsFiltersFetched(e){
     if ( !this.AppStateModel.isActivePage(this) || e.state !== 'loaded') return;
-    this.filterRows = objectUtils.chunkArray(e.payload, 4);
+    this.filters = e.payload;
   }
 
   _onFilterChange(e, filter){
@@ -114,6 +114,42 @@ export default class AppPageReports extends Mixin(LitElement)
     this.helpDialogPage = page;
     this.helpDialogRef.value.showModal();
   }
+
+  _onAggregatorChange(e, axis){
+
+    axis = axis.toUpperCase();
+    const multipleMetrics = this.selectedMetrics.length > 1;
+    const newValue = e.target.value;
+    const existingValue = this[`selectedAggregator${axis}`];
+    const otherAxisValue = this[`selectedAggregator${axis === 'X' ? 'Y' : 'X'}`];
+
+    if ( multipleMetrics && !existingValue && newValue ) {
+      this.AppStateModel.showToast({
+        type: 'error',
+        message: 'When multiple metrics are selected, only one aggregator can be used'
+      });
+      e.target.value = existingValue;
+      return;
+    }
+
+    if ( !newValue && !otherAxisValue ) {
+      this.AppStateModel.showToast({
+        type: 'error',
+        message: 'At least one aggregator must be selected'
+      });
+      e.target.value = existingValue;
+      return;
+    }
+
+    this[`selectedAggregator${axis}`] = newValue;
+  }
+
+  _onAggregatorSwap(){
+    const temp = this.selectedAggregatorX;
+    this.selectedAggregatorX = this.selectedAggregatorY;
+    this.selectedAggregatorY = temp;
+  }
+
 
 }
 
