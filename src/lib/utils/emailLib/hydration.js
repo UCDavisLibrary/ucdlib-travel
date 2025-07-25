23ef25a6fa6ad7aada81608a2c7db162374ece6f
Sabrina Baggett <sabaggett@ucdavis.edu>
2025-07-25 01:23:37 -0700

    bug fix #123

--- src/lib/utils/emailLib/hydration.js
+++ src/lib/utils/emailLib/hydration.js
@@ -24,8 +24,9 @@ export default class Hydration {
 */
 approvers(){
   const approvalStatusActivity = this.approvalRequest?.approvalStatusActivity || [];
+
   if(this.type == "request" || this.type == "next-approver") return approvalStatusActivity.filter(x => x.action == 'approval-needed');
-  if(this.type == "request-cancel") return approvalStatusActivity.filter(x => (x.action == 'approved') || (x.action == 'approved-with-changes'));
+  if(this.type == "request-cancel") return approvalStatusActivity.filter(x => (x.action == 'approve') || (x.action == 'approved-with-changes'));
   return [];
 }
 /**
@@ -123,6 +124,7 @@ _getRequesterComments(){
 */
 _getNextApproverFullName(){
   let results = this.approvers();
+
   if ( !results?.length ) return '';
   return `${results[0].employee?.firstName} ${results[0].employee?.lastName}`;
 }
@@ -133,6 +135,7 @@ _getNextApproverFullName(){
 */
 _getNextApproverFundChanges(){
   let results = this.approvers();
+
   if ( !results?.length ) return '';
   return results[0]?.fundChanges || '';
 }
@@ -143,6 +146,7 @@ _getNextApproverFundChanges(){
 */
 _getNextApproverKerberos(){
   let results = this.approvers();
+
   if ( !results?.length ) return '';
   return results[0].employeeKerberos || '';
 }
